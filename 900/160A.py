4ae3457128c7559b9c261d940ca1216383ef76cd
allacee <ddimaglushkov@ya.ru>
2021-04-01 02:44:58 +0300

    Simplify 160A solution

--- 900/160A.py
+++ 900/160A.py
@@ -1,3 +1,3 @@
 n = int(input())
 coins = sorted([int(i) for i in input().split(' ')], reverse=True)
-print([i for i, coin in enumerate(coins, 1) if sum(coins[:i]) > sum(coins[i:])][0])
+print([i for i in range(1, len(coins) + 1) if sum(coins[:i]) > sum(coins[i:])][0])
