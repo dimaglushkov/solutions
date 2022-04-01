687d30d192ba1db2eab6a027a2ee79a0429056c0
dimaglushkov <ddimaglushkov@ya.ru>
2022-04-01 21:49:55 +0400

    Add stack implementations

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dimaglushkov/solutions
+
+go 1.18
---
+++ util/stack.go
@@ -0,0 +1,30 @@
+package util
+
+type Stack[T any] struct {
+	values []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{}
+}
+
+func (s *Stack[T]) Push(val T) {
+	s.values = append(s.values, val)
+}
+
+func (s *Stack[T]) Pop() (val T, ok bool) {
+	if len(s.values) == 0 {
+		return val, false
+	}
+	val = s.values[len(s.values)-1]
+	s.values = s.values[:len(s.values)-1]
+	return val, true
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.values)
+}
+
+func (s *Stack[T]) Top() T {
+	return s.values[len(s.values)-1]
+}
