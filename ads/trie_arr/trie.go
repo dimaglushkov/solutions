7682b2dd0105862f2ee2f703784d298092813741
dimaglushkov <ddimaglushkov@ya.ru>
2022-11-06 16:56:47 +0400

    Add Trie ds

---
+++ ads/trie_arr/trie.go
@@ -0,0 +1,43 @@
+package trie_arr
+
+type Trie struct {
+	Children [26]*Trie
+	Term     bool
+}
+
+func NewTrie() *Trie {
+	return new(Trie)
+}
+
+func (t *Trie) Insert(word string) {
+	for _, ch := range word {
+		x := ch - 'a'
+		if t.Children[x] == nil {
+			t.Children[x] = NewTrie()
+		}
+		t = t.Children[x]
+	}
+	t.Term = true
+}
+
+func (t *Trie) Search(word string) bool {
+	for _, ch := range word {
+		x := ch - 'a'
+		if t.Children[x] == nil {
+			return false
+		}
+		t = t.Children[x]
+	}
+	return t.Term == true
+}
+
+func (t *Trie) StartsWith(prefix string) bool {
+	for _, ch := range prefix {
+		x := ch - 'a'
+		if t.Children[x] == nil {
+			return false
+		}
+		t = t.Children[x]
+	}
+	return true
+}
---
+++ ads/trie_map/trie.go
@@ -0,0 +1,40 @@
+package trie_map
+
+type Trie struct {
+	Children map[rune]*Trie
+	Term     bool
+}
+
+func NewTrie() *Trie {
+	return &Trie{Children: make(map[rune]*Trie)}
+}
+
+func (t *Trie) Insert(word string) {
+	for _, r := range word {
+		if t.Children[r] == nil {
+			t.Children[r] = NewTrie()
+		}
+		t = t.Children[r]
+	}
+	t.Term = true
+}
+
+func (t *Trie) Search(word string) bool {
+	for _, r := range word {
+		if t.Children[r] == nil {
+			return false
+		}
+		t = t.Children[r]
+	}
+	return t.Term == true
+}
+
+func (t *Trie) StartsWith(prefix string) bool {
+	for _, r := range prefix {
+		if t.Children[r] == nil {
+			return false
+		}
+		t = t.Children[r]
+	}
+	return true
+}
