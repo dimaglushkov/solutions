b13d8ddc6d04949f8fb694d4982dec7fbf6d95f4
dimaglushkov <ddimaglushkov@ya.ru>
2023-02-06 13:45:37 +0400

    Improve feb 6 lc dc solution

--- leetcode/shuffle-the-array/shuffle-the-array.go
+++ leetcode/shuffle-the-array/shuffle-the-array.go
@@ -7,8 +7,7 @@ import "fmt"
 func shuffle(nums []int, n int) []int {
 	res := make([]int, 0, n)
 	for i := 0; i < n; i++ {
-		res = append(res, nums[i])
-		res = append(res, nums[i+n])
+		res = append(res, nums[i], nums[i+n])
 	}
 	return res
 }
