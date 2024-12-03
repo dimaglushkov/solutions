a2b1bb1b78de5bee52c2505538d00b7612a11606
dimaglushkov <me@dimaglushkov.xyz>
2024-12-03 19:55:29 +0400

    Add AoC d3

--- adventofcode/2024/README.md
+++ adventofcode/2024/README.md
@@ -4,3 +4,4 @@
 |----------------------------------------------|-------------------------------------------------------------------------------------------------|
 | [Day 1](https://adventofcode.com/2024/day/1) | [golang](https://github.com/dimaglushkov/solutions/tree/master/adventofcode/2024/day01/main.go) | 
 | [Day 2](https://adventofcode.com/2024/day/2) | [golang](https://github.com/dimaglushkov/solutions/tree/master/adventofcode/2024/day02/main.go) | 
+| [Day 3](https://adventofcode.com/2024/day/3) | [golang](https://github.com/dimaglushkov/solutions/tree/master/adventofcode/2024/day03/main.go) | 
---
+++ adventofcode/2024/day03/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dimaglushkov/solutions/adventofcode/2024/util"
+	"regexp"
+	"strings"
+)
+
+const (
+	inputFile = "day03.input"
+	//inputFile        = "day03.example.input"
+	mulPattern       = `mul\(\d{1,3},\d{1,3}\)`
+	mulDoDontPattern = `mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`
+)
+
+func part1(input string) {
+	var res int
+
+	mulRe := regexp.MustCompile(mulPattern)
+	matches := mulRe.FindAllStringSubmatch(input, -1)
+
+	for _, m := range matches {
+		x := strings.TrimPrefix(m[0], "mul(")
+		x = strings.TrimSuffix(x, ")")
+
+		values := strings.Split(x, ",")
+		res += util.Atoi(values[0]) * util.Atoi(values[1])
+	}
+
+	fmt.Println(res)
+}
+
+func part2(input string) {
+	var res int
+
+	mulRe := regexp.MustCompile(mulDoDontPattern)
+	matches := mulRe.FindAllStringSubmatch(input, -1)
+
+	do := true
+
+	for _, m := range matches {
+		x := m[0]
+
+		switch x {
+		case "do()":
+			do = true
+		case "don't()":
+			do = false
+		default:
+			if !do {
+				break
+			}
+			x := strings.TrimPrefix(m[0], "mul(")
+			x = strings.TrimSuffix(x, ")")
+
+			values := strings.Split(x, ",")
+			res += util.Atoi(values[0]) * util.Atoi(values[1])
+		}
+	}
+
+	fmt.Println(res)
+}
+
+func main() {
+	lines := util.ReadInput(inputFile)
+
+	part1(strings.Join(lines, "\n"))
+	part2(strings.Join(lines, "\n"))
+}
